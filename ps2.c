e5a0696476e484fcec075bc581a3826167c5485f
Nicolas S. Dade <nic.dade@gmail.com>
2014-10-08 13:38:05 -0700

    fix typo in header comment

--- ps2.c
+++ ps2.c
@@ -13,7 +13,7 @@
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with ps2_kbd_to_usb_adapter,.  If not, see http://www.gnu.org/licenses/
+ *  along with ps2_kbd_to_usb_adapter.  If not, see http://www.gnu.org/licenses/
  * 
  */
 
